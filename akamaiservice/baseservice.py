fc060fed7bc25ee53315d3221b7d574163e2c6eb
micho8402 <michel.perez8402@gmail.com>
2013-05-31 19:29:53 -0400

    Adds method to retrieve service codes from the creation of the service.

--- akamaiservice/baseservice.py
+++ akamaiservice/baseservice.py
@@ -2,29 +2,48 @@ from .exception import AkamaiServiceException
 from cStringIO import StringIO
 from csv import reader as csv_reader
 
+
 class BaseService(object):
     client = None
 
-    def __init__(self, client):
+    def __init__(self, client, load_cp_codes=False):
         self.client = client
+        self.codes = None
+
+        if load_cp_codes:
+            self.get_cp_codes()
 
     def __getattr__(self, name):
         try:
             return getattr(self.client.service, name)
         except KeyError:
             message = ('%s does not exist in service %s' %
-                (name, self.__name__))
+                       (name, self.__name__))
             raise AkamaiServiceException(message)
 
+    def get_cp_codes(self):
+        """
+        Retrieves a list of CPCodeInfo instances from akamai current service.
+        CPCodeInfo format: {cpcode = 123, description = "desc", service = "Service::Method"}
+        The numeric cp codes are stored in this service instance
+        @return: list
+        """
+        data = self.invoke_method('getCPCodes')
+        self.codes = [x.cpcode for x in data]
+
+        return data
+
     def invoke_method(self, name, *args):
-        return self.parse(getattr(self, name)(*args))
+        data = getattr(self, name)(*args)
+
+        return self.parse(data) if isinstance(data, basestring) else data
 
     def parse(self, data):
         """
         Parses csv data into a list of dictionaries where every csv row
         represents an item on the list.
         @param data:
-        @return:
+        @return: list
         """
         mem_file = StringIO(data)
         reader = csv_reader(mem_file, lineterminator='\n', strict=True)
--- akamaiservice/connection.py
+++ akamaiservice/connection.py
@@ -16,6 +16,6 @@ class Connection(object):
         wsdl_url = '%s%s?wsdl' % (WSDL_BASE_URL, service_name)
         return Client(wsdl_url, username=self.username, password=self.password)
 
-    def get_service(self, service_name):
+    def get_service(self, service_name, load_cp_codes=False):
         client = self.__get_client(service_name)
-        return type(service_name, (BaseService,), dict())(client=client)
+        return type(service_name, (BaseService,), dict())(client=client, load_cp_codes=load_cp_codes)
