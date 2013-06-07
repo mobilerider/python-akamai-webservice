91bf6fd06ae51aadc73ec292aa541cd8b5af57fa
micho8402 <michel.perez8402@gmail.com>
2013-06-07 12:40:01 -0400

    Updates console client script (just for internal tests atm)

--- client.py
+++ client.py
@@ -20,48 +20,48 @@ def print_help():
 
 
 if __name__ == "__main__":
-    try:
-        if len(sys.argv) <= 3:
-            raise Exception('Missing required parameters')
+    #try:
+    if len(sys.argv) <= 3:
+        raise Exception('Missing required parameters')
 
-        opts, args = getopt.getopt(sys.argv[3:], "hc:u:p:s:e:f:m:",
-                                   ["help,codes=,username=,password=,start=,end=,format=,media="])
+    opts, args = getopt.getopt(sys.argv[3:], "hc:u:p:s:e:f:m:",
+                               ["help,codes=,username=,password=,start=,end=,format=,media="])
 
-        service_args = []
-        service_kwargs = {'service_name': sys.argv[1], 'service_method': sys.argv[2]}
+    service_args = []
+    service_kwargs = {'service_name': sys.argv[1], 'service_method': sys.argv[2]}
 
-        for opt, arg in opts:
-            if opt in ('-h', '--help'):
-                print_help()
-                sys.exit()
-            elif opt in ('-c', '--codes'):
-                service_args.append(arg.split(','))
-            elif opt in ('-u', '--username'):
-                service_kwargs['username'] = arg
-            elif opt in ('-p', '--password'):
-                service_kwargs['password'] = arg
-            elif opt in ('-s', '--start'):
-                service_args.append(datetime.strptime(arg, '%m/%d/%Y-%H:%M:%S'))
-            elif opt in ('-e', '--end'):
-                service_args.append(datetime.strptime(arg, '%m/%d/%Y-%H:%M:%S'))
-                service_args.append('GMT')  #Default time zone
-                service_args.append(None)  #Null for column array, not supported yet
-            elif opt in ('-f', '--format'):
-                f = service_kwargs.get('filter', {})
-                f['format'] = arg
-                service_kwargs['filter'] = f
-                service_args.append(f)
-            elif opt in ('-m', '--media'):
-                service_args.append(arg.split(','))
-            else:
-                raise Exception('Argument unknown')
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            print_help()
+            sys.exit()
+        elif opt in ('-c', '--codes'):
+            service_args.append(arg.split(','))
+        elif opt in ('-u', '--username'):
+            service_kwargs['username'] = arg
+        elif opt in ('-p', '--password'):
+            service_kwargs['password'] = arg
+        elif opt in ('-s', '--start'):
+            service_args.append(datetime.strptime(arg, '%m/%d/%Y-%H:%M:%S'))
+        elif opt in ('-e', '--end'):
+            service_args.append(datetime.strptime(arg, '%m/%d/%Y-%H:%M:%S'))
+            service_args.append('GMT')  #Default time zone
+            service_args.append(None)  #Null for column array, not supported yet
+        elif opt in ('-f', '--format'):
+            f = service_kwargs.get('filter', {})
+            f['format'] = arg
+            service_kwargs['filter'] = f
+            service_args.append(f)
+        elif opt in ('-m', '--media'):
+            service_args.append(arg.split(','))
+        else:
+            raise Exception('Argument unknown')
 
-        connection = Connection(service_kwargs.get('username'), service_kwargs.get('password'))
-        service = connection.get_service(service_kwargs['service_name'], True)
+    connection = Connection(service_kwargs.get('username'), service_kwargs.get('password'))
+    service = connection.get_service(service_kwargs['service_name'], True)
 
-        print service.invoke_method(service_kwargs['service_method'], *service_args, all_cp_codes=True)
+    print service.invoke_method(service_kwargs['service_method'], *service_args, all_cp_codes=True)
 
-    except Exception as ex:
-        print("Error: " + ex.msg if hasattr(ex, 'msg') else ex.message)
-        print_help()
+    #except Exception as ex:
+    #    print("Error: " + ex.msg if hasattr(ex, 'msg') else ex.message)
+    #    print_help()
 
