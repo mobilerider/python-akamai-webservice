293d99217d042aa2545415ff00b7d84d01947d8b
Mandx <gmandx@gmail.com>
2013-06-08 16:08:41 -0400

    WS_Attachment hack to make it work with Suds
    Disclaimer: THIS IS A HACK (but it does as intended)
    - Made it work by monkey-patching  no-op method `replyfilter` of the
    `suds.bindings.binding.Binding` class.
    - The monkey-patch version tries to parse the reply string, as Suds would.
    - If no SAX-parsing exception is raised, return the string untouched.
    - If a `SAXParseException` is raised, detect the payload boundaries using RegEx
      and use those payloads to construct MIME Messages and extract the the payload
      content.
    - It's assumed the first payload is the XML response, so this XML is parsed
      (using lxml) and the "return" node's text is replaced with the second
      payload's content.
    - The lxml parser is instantiated with every flag available that could
      transform the XML. When called with the default parser, some namespaces and
      prefixes where replaced.
    - The resulting XML (after replacing the text of the result node) is serialized
      as a string and then returned.

--- akamaiservice/connection.py
+++ akamaiservice/connection.py
@@ -1,7 +1,55 @@
+from email import message_from_string
+import re
+from xml.sax import SAXParseException
+from lxml.etree import XMLParser, fromstring as xml_from_string, tostring as xml_to_string
+
 from suds.client import Client
+from suds.sax.parser import Parser
+from suds.bindings.binding import Binding
 
 from .constants import WSDL_BASE_URL
 from .baseservice import BaseService
+
+
+BOUNDARY_REGEX = re.compile('^-+=(?P<boundary_name>[^-]+)-*$')
+
+
+def replyfilter(self, reply_string):
+    try:
+        Parser().parse(string=reply_string)
+        return reply_string
+    except SAXParseException:
+        parts = []
+        part = []
+        for l in reply_string.splitlines():
+            if not BOUNDARY_REGEX.match(l):
+                part.append(l)
+            else:
+                parts.append('\n'.join(part))
+                part = []
+        parts = [
+            message_from_string(_p).get_payload()
+            for _p in parts
+            if ''.join(_p.splitlines()).strip()
+        ]
+        xml = xml_from_string(parts[0], parser=XMLParser(
+            attribute_defaults=False,
+            dtd_validation=False,
+            load_dtd=False,
+            no_network=False,
+            ns_clean=False,
+            remove_blank_text=False,
+            remove_comments=False,
+            remove_pis=False,
+            strip_cdata=False,
+            compact=False,
+            resolve_entities=False,
+
+        ))
+        xml.find('.//getLiveStreamTrafficForCPCodeV2Return').text = parts[1]
+        return xml_to_string(xml)
+
+Binding.replyfilter = replyfilter
 
 
 class Connection(object):
